39ba2829a07c440c1c1cc0b0a6cdaecfa490e8dd
Pita <clone99t@gmail.com>
2022-01-29 16:35:06 -0500

    Void sound

--- Scripts/void.gd
+++ Scripts/void.gd
@@ -4,5 +4,6 @@ onready var player = get_tree().current_scene.get_node("Player")
 
 func _on_Void_body_entered(body):
 	player.queue_free()
+	$DeathSound.play()
 	yield(get_tree().create_timer(2.0), "timeout")
 	get_tree().reload_current_scene()
