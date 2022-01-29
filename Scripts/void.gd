7eb7f94ed678298a7d418cf7790b4d538ea26844
Pita <clone99t@gmail.com>
2022-01-29 14:43:12 -0500

    Added a void

---
+++ Scripts/void.gd
@@ -0,0 +1,8 @@
+extends Area2D
+
+onready var player = get_tree().current_scene.get_node("Player")
+
+func _on_Void_body_entered(body):
+	player.queue_free()
+	yield(get_tree().create_timer(2.0), "timeout")
+	get_tree().reload_current_scene()
