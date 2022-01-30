a0ce8a9daaf8a711d5e6ce8d45921d49275d7ae2
844958340405984 <ralbrecht@cedarville.edu>
2022-01-30 17:32:01 -0500

    fix

--- Scripts/SceneChanger.gd
+++ Scripts/SceneChanger.gd
@@ -8,7 +8,6 @@ func change_scene(path, delay = 0.5):
 	yield(get_tree().create_timer(delay), "timeout")
 	animation_player.play("fade_to_black")
 	yield(animation_player, "animation_finished")
-	assert(get_tree().change_scene(path) == OK)
 	animation_player.play_backwards("fade_to_black")
 	emit_signal("scene_changed")
 
